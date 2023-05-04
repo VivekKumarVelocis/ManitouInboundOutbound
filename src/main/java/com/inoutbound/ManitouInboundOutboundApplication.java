9e82d336eb5b466ac4c2247cb0ab7433385ec9bb
Vivek <vivek.kumar@velocis.co.in>
2023-05-04 10:02:09 +0530

    First Commit

---
+++ src/main/java/com/inoutbound/ManitouInboundOutboundApplication.java
@@ -0,0 +1,13 @@
+package com.inoutbound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManitouInboundOutboundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManitouInboundOutboundApplication.class, args);
+	}
+
+}
